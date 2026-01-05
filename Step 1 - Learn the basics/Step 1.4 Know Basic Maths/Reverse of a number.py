05c68ca38341f4db43a5715e44795026117cd24c
DineshMSDian <dinesh2742004@gmail.com>
2026-01-05 10:38:12 +0530

    Step 1 - Learn the basics/Step 1.4 Know Basic Maths
    problems Solved:
    - Count all digits of a number
    - Reverse of a number

---
+++ Step 1 - Learn the basics/Step 1.4 Know Basic Maths/Reverse of a number.py
@@ -0,0 +1,11 @@
+def ReverseNumber(n):
+    revNum = 0
+
+    while n > 0:
+        last_digit = n % 10
+        revNum = (revNum * 10) + last_digit
+        n = n // 10
+    return revNum
+
+n = int(input("Enter N value: "))
+print(ReverseNumber(n))
